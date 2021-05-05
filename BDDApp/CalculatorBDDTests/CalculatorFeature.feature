bcad2a658b9000732fe5a16327074a6c4e3fc839
Dylan Lewis <Dlewis@spartaglobal.com>
2021-05-05 11:05:40 +0100

    Initial commit

---
+++ BDDApp/CalculatorBDDTests/CalculatorFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: CalculatorFeature
+	Simple calculator for adding two numbers
+	In order to avoid silly mistakes
+	As a maths idiot
+	I want to be able to do simple calculations
+
+@mytag
+Scenario: Add two numbers
+	Given I have a calculator
+	And I enter 5 and 2 into the calculator
+	When I press add
+	Then the result should be 7
---
+++ BDDApp/CalculatorBDDTests/CalculatorFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace CalculatorBDDTests
+{
+    [Binding]
+    public class CalculatorFeatureSteps
+    {
+        [Given(@"I have a calculator")]
+        public void GivenIHaveACalculator()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter (.*) and (.*) into the calculator")]
+        public void GivenIEnterAndIntoTheCalculator(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
